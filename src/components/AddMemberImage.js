a0f1bf776bd0b258e7a514b874db270b4e40b39d
Samuel Li <samuelli4521@gmail.com>
2019-05-17 17:27:14 -0700

    removed file size limit

--- src/components/AddMemberImage.js
+++ src/components/AddMemberImage.js
@@ -19,11 +19,7 @@ function beforeUpload(file) {
   if (!isJPG) {
     message.error('You can only upload JPG file!');
   }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJPG && isLt2M;
+  return isJPG;
 }
 
 class AddMemberImage extends Component {
